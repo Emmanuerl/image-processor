31c90848c4a3ad7094862c7d12b3a8cf052cdcb8
Chukwuemeka Chukwurah <emekaemmanuel045@gmail.com>
2022-06-18 11:17:26 +0100

    switch from npm to yarn add use axios to download images

--- src/controllers/images/images.controller.ts
+++ src/controllers/images/images.controller.ts
@@ -1,4 +1,4 @@
-import { deleteLocalFiles, filterImageFromURL } from "../../../util/util";
+import { deleteLocalFiles, filterImageFromURL } from "../../util/util";
 import express, { Request, Response } from "express";
 
 import { isFilterImageDTO } from "./images.validator";
@@ -6,16 +6,16 @@ import { isFilterImageDTO } from "./images.validator";
 export const router = express.Router();
 
 router.get("/", isFilterImageDTO, async (req: Request, res: Response) => {
-  const { image_url } = req.query;
-  const filteredImage = await filterImageFromURL(image_url);
-
-  res.sendFile(filteredImage, (err) => {
+  const url = req.query.image_url as string;
+  const images = await filterImageFromURL(url);
+  console.log(images);
+  res.sendFile(images[0], (err) => {
     if (err)
       return res
         .send(500)
         .send(
           "We are currently having system level issues. Kindly bear with us."
         );
-    deleteLocalFiles([filteredImage]);
+    deleteLocalFiles(images);
   });
 });
